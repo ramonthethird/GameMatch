8140f2b4e2bef72795848cb65758d9b6687616a7
Ramon <ramonquintana619@gmail.com>
2024-10-13 22:56:59 -0700

    add comments to all API related files

--- game_match/lib/pages/api_service.dart
+++ game_match/lib/pages/api_service.dart
@@ -4,27 +4,36 @@ import 'package:http/http.dart' as http;
 import 'game_model.dart';
 
 class ApiService {
+  // Instance of FlutterSecureStorage for storing sensitive info securely
   final FlutterSecureStorage secureStorage = FlutterSecureStorage();
+
+  // Twitch API client credentials
   final String clientId = 'v5v1uyyo05m4ttc8yvd26yrwslfimc';
   final String clientSecret = 'hu3w4pwpc344uwdp2k77xfjozbaxc5';
+
+  // Base URL for IGDB API
   final String baseUrl = 'https://api.igdb.com/v4';
 
+  // Function to store the access token
   Future<void> storeAccessToken(String token) async {
     print('Storing access token: $token');
     await secureStorage.write(key: 'access_token', value: token);
   }
 
+  // Function to retrieve the access token from secure storage
   Future<String?> retrieveAccessToken() async {
     final token = await secureStorage.read(key: 'access_token');
     print('Retrieved access token: $token');
     return token;
   }
 
+  // Function to autheticate and get an access token from the API
   Future<void> authenticate() async {
     final Uri url = Uri.parse(
         'https://id.twitch.tv/oauth2/token?client_id=$clientId&client_secret=$clientSecret&grant_type=client_credentials');
 
     try {
+      // POST request to fetch the access token
       final response = await http.post(url);
 
       print('Authentication Response: ${response.body}');
@@ -43,15 +52,19 @@ class ApiService {
     }
   }
 
+  // Function to fetch a list of games from the IGDB API
   Future<List<Game>> fetchGames() async {
+    // Retrieve access token from secure storage
     final String? accessToken = await retrieveAccessToken();
 
+    // If no token available authenticate and fetch a new token
     if (accessToken == null) {
       print('Access token is null. Please fetch a new one.');
       await authenticate(); // Authenticate if the token is not available
       return [];
     }
 
+    // API endpoint to fetch game data
     final Uri url = Uri.parse('https://api.igdb.com/v4/games');
     final String body = '''
     fields name, summary, genres.name, cover.url, platforms.name, release_dates.human, websites.url;
@@ -74,9 +87,13 @@ class ApiService {
       print('Response Status Code: ${response.statusCode}');
       print('Response Body: ${response.body}');
 
+      // If request is successful
       if (response.statusCode == 200) {
         print('Response Body: ${response.body}');
+        // Decode the response body into a list of dynamic JSON objects
         final List<dynamic> gameDataJson = json.decode(response.body);
+
+        // Convert JSON objects into a list of Game objects
         final List<Game> games = gameDataJson
             .map((dynamic json) => Game.fromJson(json as Map<String, dynamic>))
             .toList();
