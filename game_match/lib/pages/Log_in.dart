8742b60b6256e46f15a811084008b282ecec100f
Jacob Phillips <113879820+jxke66@users.noreply.github.com>
2024-10-13 23:27:12 -0700

    Update Log_in.dart

---
+++ game_match/lib/pages/Log_in.dart
@@ -0,0 +1,205 @@
+// still needs to edit for consolidation
+
+
+import 'package:flutter/material.dart';
+import 'package:login_ui_1/recoverusername.dart';  // Import the recovery page
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Basic Login',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: const MyLoginPage(title: 'My Main Login Page'),
+    );
+  }
+}
+
+class MyLoginPage extends StatefulWidget {
+  const MyLoginPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyLoginPage> createState() => _MyLoginPageState();
+}
+
+class _MyLoginPageState extends State<MyLoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Image.asset(
+                  'images/gamematchlogoresize.png',
+                  height: 260,
+                  width: 260,
+                ),
+              ),
+
+              const Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(height: 32),
+
+              SizedBox(
+                height: 40,
+                width: 325,
+                child: TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Enter your Username',
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  ),
+                ),
+              ),
+              
+              const SizedBox(height: 2),
+
+  
+              GestureDetector(
+                onTap: () {
+                  // navigate to the RecoverUsernamePage
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const UsernameRecoveryPage()), // Ensure RecoverUsernamePage is imported and declared
+                  );
+                },
+
+                
+                child: const Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Forgot your Username?',
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.blue, // blue color
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 32),
+              SizedBox(
+                height: 40,
+                width: 325,
+                child: TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Enter your Password',
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  ),
+                ),
+              ),
+
+              
+              const SizedBox(height: 2),
+              const Padding(
+                padding: EdgeInsets.only(left: 30),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Forgot your Password?',
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+
+              
+              const SizedBox(height: 40),
+
+              // const SizedBox(height: 20),
+
+
+
+              Container(
+                width: 140,
+                height: 30,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                  borderRadius: BorderRadius.all(Radius.circular(2.5)),
+                ),
+
+                
+                child: const Center(
+                  child: Text(
+                    'continue',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ),
+
+              
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account? ",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      // Add sign-up functionality here
+                    },
+                    child: const Text(
+                      'Sign up',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
