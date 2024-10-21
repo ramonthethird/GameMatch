ab606e912f8a416b03dc50aeb6464167a0556997
Jacob Phillips <113879820+jxke66@users.noreply.github.com>
2024-10-20 23:56:59 -0700

    Create recoverpassword.dart
    send email for password reset should work. Need to upgrade front end though and polish it up

---
+++ game_match/lib/pages/recoverpassword.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class PasswordRecoveryPage extends StatefulWidget {
+  const PasswordRecoveryPage({super.key});
+
+  @override
+  State<PasswordRecoveryPage> createState() => _PasswordRecoveryPageState();
+}
+
+class _PasswordRecoveryPageState extends State<PasswordRecoveryPage> {
+  final TextEditingController _emailController = TextEditingController();
+
+  // Send Password Reset Function
+  Future<void> _sendPasswordResetEmail() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: _emailController.text);
+
+      // Success message here
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Password reset email sent!')),
+      );
+      // Give option for navigate back or clear the text field (from doc)
+      _emailController.clear();
+    } catch (e) {
+
+      // Show default error message when error happens
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: ${e.toString()}')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Recover Password'),
+      ),
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Image.asset(
+                  'assets/images/gamematchlogoresize.png',
+                  height: 260,
+                  width: 260,
+                ),
+              ),
+
+
+              const SizedBox(height: 16),
+
+
+              Container(
+                alignment: Alignment.center,
+                child: const Text(
+                  'Recover your Password',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+
+
+              const SizedBox(height: 10),
+
+
+              const Text(
+                'Please enter the email associated with your account. Your password will be emailed to you shortly.',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black87,
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+
+
+              const SizedBox(height: 32),
+
+
+              SizedBox(
+                height: 40,
+                width: 325,
+                child: TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Enter your Email',
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _sendPasswordResetEmail, // Send password reset email here
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.lightBlueAccent, 
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(2.5), // Cohesive check
+                  ),
+                  fixedSize: const Size(140, 30),
+                ),
+
+                child: const Center(
+                  child: Text(
+                    'Get Password',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
